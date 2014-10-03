986f20fda16469e23b9c662cdeaadde79ac464e2
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2014-10-03 12:59:08 +0300

    fix paths in middleware bootstrap

--- src/bootstrapMiddleware.js
+++ src/bootstrapMiddleware.js
@@ -5,10 +5,10 @@ function BootstrapMiddleware(app){
         throw new Error('Invalid argument');
     }
 
-    app.set('views', __dirname + '/views');
+    app.set('views', __dirname + '/../views');
     app.set('view engine', 'jade');
 
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/../public'));
 }
 
 module.exports = BootstrapMiddleware;
