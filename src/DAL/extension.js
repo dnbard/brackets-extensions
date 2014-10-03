e8a416cf03408bfe82da70e095a86d9c9482db9d
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2014-10-03 16:42:15 +0300

    layout and data for index page

--- src/DAL/extension.js
+++ src/DAL/extension.js
@@ -12,4 +12,22 @@ ExtensionDAL.prototype.getExtensionsCount = function(){
     });
 }
 
+ExtensionDAL.prototype.getNewestExtension = function(){
+    return this.cached('extensionNewest', function(){
+        return Extension.findOne({}).sort({timestamp: -1}).lean().exec();
+    });
+}
+
+ExtensionDAL.prototype.getMostDownloadsExtension = function(){
+    return this.cached('extensionDownloads', function(){
+        return Extension.findOne({}).sort({totalDownloads: -1}).lean().exec();
+    });
+}
+
+ExtensionDAL.prototype.getMostDownloadsExtensionList = function(){
+    return this.cached('extensionDownloadsLIst', function(){
+        return Extension.find({}).sort({totalDownloads: -1}).limit(12).lean().exec();
+    });
+}
+
 module.exports = new ExtensionDAL();
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,18 +1,30 @@
 var winston = require('winston'),
+    _ = require('lodash'),
     ExtensionDAL = require('../DAL/extension'),
     Q = require('q');
 
 function IndexController(){}
 
-IndexController.prototype.default = this.default = function(req, res, next){
-    Q.all([ExtensionDAL.getExtensionsCount()]).then(function(results){
-        var count = results[0];
+IndexController.prototype.default = function(req, res, next){
+    Q.all([
+        ExtensionDAL.getExtensionsCount(),
+        ExtensionDAL.getNewestExtension(),
+        ExtensionDAL.getMostDownloadsExtension(),
+        ExtensionDAL.getMostDownloadsExtensionList()
+    ]).then(_.bind(function(result){
+        var count = result[0],
+            newestExtension = result[1],
+            downloadsExtension = result[2],
+            downloadsExtensionList = result[3];
 
         res.render('index', {
             title : 'Home',
-            count: count
+            count: count,
+            newestExtension: newestExtension,
+            downloadsExtension: downloadsExtension,
+            downloadsExtensionList: downloadsExtensionList
         });
-    }, function(){
+    }, this), function(){
         res.status(500).send();
     });
 }
