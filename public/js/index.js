2b1be99e21e3839af62070e54b89f226a22be116
Alex Bardanov <dnbard@gmail.com>
2014-10-20 22:50:40 +0300

    layout changes in index page

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,8 @@ IndexPageViewModel.prototype.initNumbers = function(element){
 
         if (initial > 1000){
             result = Math.floor(parseInt(initial) / 1000) + 'k';
+        } else if (initial < 1000){
+            result = Math.floor(initial / 10) * 10;
         }
 
         return result;
